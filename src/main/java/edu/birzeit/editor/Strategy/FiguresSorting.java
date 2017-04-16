f7ecd129a82fdaaad72dcb0eae57fc2532df0e32
Rawan-Khader <rawan.khader.94@gmail.com>
2017-04-16 12:23:14 +0300

    StrategyPattern sort

--- src/main/java/edu/birzeit/editor/Strategy/FiguresSorting.java
+++ src/main/java/edu/birzeit/editor/Strategy/FiguresSorting.java
@@ -3,10 +3,35 @@
  */
 package edu.birzeit.editor.Strategy;
 
+import java.util.List;
+
+import org.jhotdraw.draw.Figure;
+
+
 /**
  * @author Rawan Khader
  *
  */
 public class FiguresSorting {
 
+	private SortFigures sortFigures;
+
+	/**
+	 * @return the sortFigures
+	 */
+	public SortFigures getSortFigures() {
+		return sortFigures;
+	}
+
+	/**
+	 * @param sortFigures the sortFigures to set
+	 */
+	public void setSortFigures(SortFigures sortFigures) {
+		this.sortFigures = sortFigures;
+	}
+
+	
+	public void sort(List<Figure> figures){
+		this.sortFigures.sortAllFigures(figures);
+	}
 }
