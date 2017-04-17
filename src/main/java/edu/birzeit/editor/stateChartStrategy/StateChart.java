970c4e079fddf8a1a2ed310a64da883b6b8f5fc3
Rawan-Khader <rawan.khader.94@gmail.com>
2017-04-17 16:56:34 +0300

    Salah's Modification

--- src/main/java/edu/birzeit/editor/stateChartStrategy/StateChart.java
+++ src/main/java/edu/birzeit/editor/stateChartStrategy/StateChart.java
@@ -5,6 +5,7 @@ package edu.birzeit.editor.stateChartStrategy;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Iterator;
 
 import org.jhotdraw.draw.Figure;
 
@@ -16,8 +17,9 @@ import org.jhotdraw.draw.Figure;
 /**
  * StateChart is a concrete strategy 
  */
-public class StateChart extends StateChartStepItem {
-
+public class StateChart extends StateChartStepItem implements Iterator<StateChartStepItem> {
+	private int index = 0;
+	
 	/**
 	 * Figure representation
 	 */
@@ -27,6 +29,15 @@ public class StateChart extends StateChartStepItem {
 	 * Composition relation Child List
 	 */
 	private List<StateChartStepItem> stepItems = new LinkedList<StateChartStepItem>();
+	private Iterator<StateChartStepItem> stepItemsIterator = stepItems.listIterator();
+	
+	
+	/* 
+	 * Used to return iterator object for StateChartStepItem objects.
+	 */
+	public Iterator<StateChartStepItem> interator() {
+		return stepItems.listIterator();
+	}
 	
 	/**
 	 * 
@@ -98,6 +109,67 @@ public class StateChart extends StateChartStepItem {
 	public List<StateChartStepItem> getStepItems() {
 		//Auto-generated method stub
 		return stepItems;
+	}
+
+	/* 
+	 * Returns true if the iteration has more elements.
+	 */
+	@Override
+	public boolean hasNext() {
+		return stepItemsIterator.hasNext();
+		
+		
+		// OR we can implement it again as shown below:
+		/*
+		boolean hasNextState = false;
+		
+		try {
+			if (stepItems.get(index) != null) hasNextState = true;
+		} catch (IndexOutOfBoundsException e) {
+			hasNextState = false;
+		}
+		
+		return hasNextState;*/
+	}
+
+	/* 
+	 * Returns the next element in the iteration.
+	 */
+	@Override
+	public StateChartStepItem next() {
+		return stepItemsIterator.next();
+		
+		
+		// OR we can implement it again as shown below:
+		/*
+		StateChartStepItem stateChartStepItem = null;
+		
+		try {
+			stateChartStepItem = stepItems.get(index);
+			index = index + 1;
+		} catch (IndexOutOfBoundsException e) {
+			stateChartStepItem = null;
+		}
+		
+		return stateChartStepItem;*/
+	}
+
+	/* 
+	 * Removes from the underlying Collection the last element returned by the Iterator (optional operation).
+	 */
+	@Override
+	public void remove() {
+		stepItemsIterator.remove();
+
+		
+		// OR we can implement it again as shown below:
+		/*
+		try {
+			stepItems.remove(index);
+			index = index - 1;
+		} catch (IndexOutOfBoundsException e) {
+			// to log.
+		}	*/
 	}
 
 }
